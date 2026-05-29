b270c856cf47173be1f9778f622db51762f13b26
Thabel <thabelkodjo@gmail.com>
2026-05-29 16:24:54 +0000

    fix: Correct ALLOWED_HOSTS parsing to handle comma-separated values

--- app/app/settings.py
+++ app/app/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY =  os.environ.get("SECRET_KEY")
 DEBUG = bool(os.environ.get("DEBUG", default=1))
 
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "").split(",")
 
 
 # Application definition
